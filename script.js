ba0cb6d4be4d15a4c6d2317c85d7c659ecf639a9
eryc_s.z.w <erycszw10@gmail.com>
2023-01-18 14:16:17 +0700

    add delete and read toggle feature

--- script.js
+++ script.js
@@ -2,37 +2,18 @@ let library = [];
 
 const container = document.getElementById('container');
 const submit = document.getElementById('submit')
-const add = document.getElementById('add')
-
-//modal
-let modal = document.getElementById("myModal");
-let span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-    modal.addEventListener('mouseover', ()=> {
-        modal.style.cursor = 'pointer'
-    })
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
-add.addEventListener('click', ()=> {
-    modal.style.display = "block";
-})
 
 //form
 submit.addEventListener('click',(event)=> {
     event.preventDefault();
-    modal.style.display = "visible";
-    library.push(getBook());
-    console.log(library)
-    for(const element of library) {
-        addCard(element);
+    if(getBook() === undefined) {
+        return
+    } else {
+        library.push(getBook());
     }
+    console.log(library)
+    addCard(library[library.length-1])
+
 })
 
 //Object Book and function
@@ -48,19 +29,69 @@ function getBook() {
     let author = document.getElementById('author').value
     let page = document.getElementById('pages').value
     let read = document.getElementById('read').checked
-    return new Book(title,author,page,read)
+    if(title === '' || author === '' || page === '') {
+        alert('please fill the form correctly!')
+    } else {
+        return new Book(title,author,page,read)
+    }
 }
 
 function addCard(obj){
+    //define variable stage
     const card = document.createElement('div');
-    let judul = document.createElement('p');
+    let img = document.createElement('img');
+    img.setAttribute('src','/book.jpg')
+    let judul = document.createElement('h3');
     judul.textContent = obj.title;
     let penulis = document.createElement('p');
-    penulis.textContent = obj.author;
+    penulis.textContent = `by : ${obj.author}`;
     let halaman = document.createElement('p');
-    halaman.textContent = obj.pages
+    halaman.textContent = `${obj.pages} pages`;
+    let del = document.createElement('i');
+    let icon = document.createElement('div');
+    icon.classList.add('icon');
+    let read = document.createElement('i');
+    del.setAttribute('class','fa-solid fa-trash');
+    del.style.color = 'red';
+
+    //Check wether user already read the book or not
+    obj.isRead === true? read.setAttribute('class','fa-solid fa-check'):read.setAttribute('class','fa-solid fa-xmark');
+    obj.isRead === true? read.style.color = 'green':read.style.color='red'
+
+    //toggle read script
+    read.addEventListener('click',()=> {
+        obj.isRead = !obj.isRead;
+        if(obj.isRead === false) {
+            read.removeAttribute('class')
+            read.setAttribute('class','fa-solid fa-xmark');
+            read.style.color='red'
+        } else {
+            read.removeAttribute('class')
+            read.setAttribute('class','fa-solid fa-check');
+            read.style.color='green'
+        }
+        console.log(obj.isRead)
+    })
+
+    //append stage
+    icon.appendChild(read);
+    icon.appendChild(del);
+    card.appendChild(img)
     card.appendChild(judul);
     card.appendChild(penulis);
     card.appendChild(halaman);
+    card.appendChild(icon)
+    card.classList.add('card')
     container.appendChild(card);
+
+    //delete function
+    del.addEventListener('click', ()=> {
+        console.log(library.indexOf(obj));
+        let curr = library.indexOf(obj);
+        // console.log(container.children[curr])
+        container.removeChild(container.children[curr]);
+        library.splice(curr,1);
+    })
 }
+
+
